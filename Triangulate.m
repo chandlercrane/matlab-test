1e7fd236c280dd37ee1e14d4670f2dfd72643c2e
Chandler <ccrane2@nd.edu>
2020-10-20 15:47:12 -0400

    Update Triangulate.m

--- Triangulate.m
+++ Triangulate.m
@@ -1,4 +1,4 @@
-%% Chapter 1, Problem 16 (Triangulation)
+%% Triangulation
 % Triangulate.m
 %
 % Calculates the height of a tree given an angle, theta, and a 5 ft tripod
@@ -7,19 +7,15 @@
 % inclinometer and the angle and calculates and displays the height of the
 % tree
 %
-% Author: Chandler Crane
-
-clear
-clc
 
 %% Assigning Variables
 
 d = input('What is the distance to the tree? (feet)     '); % Distance of inclinometer from the tree
 theta = input('What is the angle to the tree top? (degrees)     '); % What is theta?
-
+tripodHeight = 5;
 
 %% Calculating Tree Top Height
 
-treeHeight = 5 + (tand(theta)*d); % Calculates tree height by trig identity for finding opposite side and accounting for 5 foot tall tripod
+treeHeight = tripodHeight + (tand(theta)*d); % Calculates tree height by trig identity for finding opposite side and accounting for 5 foot tall tripod
 
 disp(['The tree''s height is ', num2str(treeHeight), ' feet tall!']);
